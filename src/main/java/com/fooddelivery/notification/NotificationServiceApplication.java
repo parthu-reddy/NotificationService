b22a4d2bdc33ddd7f4ce4d64f1acd4b42f0a862f
Parthu <parthudhanireddy@gmail.com>
2026-08-22 15:37:21 +0530

    Fix schema validation and configuration errors

--- src/main/java/com/fooddelivery/notification/NotificationServiceApplication.java
+++ src/main/java/com/fooddelivery/notification/NotificationServiceApplication.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.context.annotation.Bean;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@SpringBootApplication(scanBasePackages = {"com.fooddelivery.notification", "com.fooddelivery.common", "com.fooddelivery"})
-@EntityScan(basePackages = {"com.fooddelivery.notification", "com.fooddelivery.common"})
-@EnableJpaRepositories(basePackages = {"com.fooddelivery"})
+@SpringBootApplication(
+    scanBasePackages = {"com.fooddelivery.notification", "com.fooddelivery.common"}
+)
+@org.springframework.boot.autoconfigure.domain.EntityScan(basePackages = {"com.fooddelivery.notification", "com.fooddelivery.common.entity"})
+@org.springframework.data.jpa.repository.config.EnableJpaRepositories(basePackages = {"com.fooddelivery.notification", "com.fooddelivery.common.repository"})
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
