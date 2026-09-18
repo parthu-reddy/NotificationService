499d2405487158d2f674eff5c11a981c34b701c1
Parthu <parthudhanireddy@gmail.com>
2026-09-18 08:23:19 +0530

    Fix contract test BaseMessagingClasses and update distributed lock implementations

--- src/test/java/com/fooddelivery/notification/contract/AdNotificationConsumerContractTest.java
+++ src/test/java/com/fooddelivery/notification/contract/AdNotificationConsumerContractTest.java
@@ -42,7 +42,7 @@ import static org.mockito.Mockito.verify;
 @ActiveProfiles("contract-test")
 @AutoConfigureStubRunner(ids = "com.fooddelivery:campaign-service:+:stubs")
 @org.springframework.test.annotation.DirtiesContext(classMode = org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_CLASS)
-@EmbeddedKafka(partitions = 1, topics = {"ad-events"})
+@EmbeddedKafka(adminTimeout = 60, partitions = 1, topics = {"ad-events"})
 class AdNotificationConsumerContractTest {
 
     @org.springframework.boot.SpringBootConfiguration
